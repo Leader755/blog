e1a8a6bf9544d314a5150a690b5f249b7c6fda69
zymseo <1047832475@qq.com>
2017-03-07 17:54:29 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+    header('Content-type:text/html;charset="utf8"');
+    
+    define('APP_NAME', 'APP');
+    define('APP_PATH', './APP/');
+    define('APP_DEBUGE', TRUE);
+    
+    include './ThinkPHP/ThinkPHP.php';
